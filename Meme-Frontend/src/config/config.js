78f1209333b403b6a1a13453cb3c3f4e1b32dbd8
ignor <guptaayodhya369@gmail.com>
2026-01-29 00:50:29 +0530

    done

--- Meme-Frontend/src/config/config.js
+++ Meme-Frontend/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   API_BASE_URL:
-    import.meta.env.VITE_API_URL || "https://backend-9qrk.onrender.com",
+    import.meta.env.VITE_API_URL || "https://backend-9qkr.onrender.com",
   WS_BINANCE_URL:
     import.meta.env.VITE_WS_BINANCE_URL ||
     "wss://stream.binance.com:9443/ws/btcusdt@trade",
